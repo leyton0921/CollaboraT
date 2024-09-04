6fc6f61d16ec03cd8451c5f0accbbef212bf18d4
brandonprogram4 <brandonprogram4@gmail.com>
2024-09-04 10:43:04 -0500

    create navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import Navbar from "./UI/navbar/nav";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-  
+      <Navbar className={"center"} ></Navbar>  …
+      
     </main>
   );
 }
