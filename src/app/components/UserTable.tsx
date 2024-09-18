2c7ebb5b4107bde51b10e9d672d2aa14ac808237
leyton0921 <leytoncarmonaisabella@gmail.com>
2024-09-18 09:12:16 -0500

    drivers are added to connect to the api

--- src/app/components/UserTable.tsx
+++ src/app/components/UserTable.tsx
@@ -1,11 +1,42 @@
-import { useSelector } from 'react-redux';
-import { RootState } from '../store/store';
+import React, { useEffect, useState } from 'react';
+import { fetchUsers } from '../controllers/getUsers.controller';
 import styles from '../styles/UserTable.module.css';
 
-const UserTable = () => {
-  const users = useSelector((state: RootState) => state.users.users);
-  console.log(users)
+interface User {
+  id: string;
+  name: string;
+  role: string;
+  tasks: { id: string; title: string }[];
+}
 
+const UserTable = () => {
+  const [users, setUsers] = useState<User[]>([]); //  almacenar los usuarios
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    // obtener los usuarios 
+    const loadUsers = async () => {
+      setLoading(true);
+      const data = await fetchUsers();
+      if (data.length > 0) {
+        setUsers(data); // actualiza los usuarios con los datos recibidos
+      } else {
+        setError('No users found or failed to fetch');
+      }
+      setLoading(false);
+    };
+
+    loadUsers();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
 
   return (
     <div className={styles.container}>
@@ -19,15 +50,15 @@ const UserTable = () => {
           </tr>
         </thead>
         <tbody>
-          {users.map((user, index) => (
-            <tr key={index}>
+          {users.map((user) => (
+            <tr key={user.id}>
               <td className="flex items-center space-x-4 py-4 pl-4">
                 <span className={styles.avatar}>
                   {user.name.charAt(0).toUpperCase()}
                 </span>
                 <span>{user.name}</span>
               </td>
-              <td>{user.role || "no role"}</td>
+              <td>{user.role || "No role"}</td>
               <td className={styles.taskCount}>{user.tasks.length || 0}</td>
               <td className="py-4 text-center">
                 <input type="checkbox" className={styles.checkbox} />
--- src/app/store/slices/tasksSlice.tsx
+++ src/app/store/slices/tasksSlice.tsx
@@ -1,7 +1,7 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 export interface Task {
-  id: string;  // UUID
+  id: string;  
   name: string;  
   description?: string; 
   dueDate: string;  
