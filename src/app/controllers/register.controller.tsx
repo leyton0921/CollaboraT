83f79f3dcf95ab6797ca2251657ecd11a083dca8
ssebastianvv <ssebastianvapi@gmail.com>
2024-09-18 06:49:06 -0500

    change register

--- src/app/controllers/register.controller.tsx
+++ src/app/controllers/register.controller.tsx
@@ -1,26 +1,27 @@
+import { IUserCompanies } from '../interface/auth.interface';
 
-import { IUserCompanies,IAuthStateCompanies } from '../interface/auth.interface';
-
-export const registerUser = async (name: string, email: string, password: string, nit:string ): Promise<{ user: IUserCompanies | null, message: string }> => {
+export const registerUser = async (
+  name: string,
+  email: string,
+  password: string,
+  nit: string
+): Promise<{ user: IUserCompanies | null; message: string }> => {
   try {
     const response = await fetch('http://localhost:4000/api/v1/auth/register/company', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ name, email, password,nit }),
+      body: JSON.stringify({ name, email, password, nit }),
     });
 
-    const data: IAuthStateCompanies | { message?: string } = await response.json();
-    console.log(data);
+    const data: IUserCompanies | { message?: string } = await response.json();
 
     if (response.ok) {
-      if ('token' in data && 'name' in data) {
-        const { user, token } = data as IAuthStateCompanies;
-        localStorage.setItem('token', token);
-        return { user, message: 'Registration successful' };
-      }
-      return { user: null, message: 'Unexpected response format' };
+      const user = data as IUserCompanies; 
+      localStorage.setItem('user', user.name);
+      return { user, message: 'Registration successful' };
     } else {
-      return { user: null, message: (data as { message?: string }).message || 'Registration failed' };
+      const message = (data as { message?: string }).message || 'Registration failed';
+      return { user: null, message };
     }
   } catch (error) {
     const errorMessage = error instanceof Error ? error.message : 'Unknown error occurred';
